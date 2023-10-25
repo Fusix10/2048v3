0481f4cca1da7205fce4386ab62275f0b3bb40f6
skybloc429 <sto.lukas18@gmail.com>
2023-10-25 15:52:56 +0200

    deb

--- 2048v3/2048v3.cpp
+++ 2048v3/2048v3.cpp
@@ -3,13 +3,45 @@
 
 #include <iostream>
 
+#include <conio.h>
+
+#define KEY_UP 72
+#define KEY_DOWN 80
+#define KEY_LEFT 75
+#define KEY_RIGHT 77
+
+void deplace() {
+    bool badKey = true;
+    while (badKey)
+    {
+        badKey = false;
+        int c = 0;
+        switch ((c = _getch()))
+        {
+        case KEY_UP:
+            std::cout << "haut\n";
+            break;
+        case KEY_DOWN:
+            std::cout << "bas\n";
+            break;
+        case KEY_RIGHT:
+            std::cout << "droit\n";
+            break;
+        case KEY_LEFT:
+            std::cout << "gauche\n";
+            break;
+        default:
+            badKey = true;
+            break;
+        }
+    }
+}
+
 int main()
 {
 	int r = true;
 	while (r) 
 	{
-
+        deplace();
 	}
-   std::cout << "Hello World!OPEN\n";
-
 }
