bd16352fd9429f101f5c91a4c7744497f2aadac7
skybloc429 <sto.lukas18@gmail.com>
2023-11-03 10:59:33 +0100

    fzeaz

--- 2048v3/2048v3.cpp
+++ 2048v3/2048v3.cpp
@@ -17,14 +17,13 @@ int main(int argc, char** args)
 {
 	Window* window = new Window;
 	window->Create();
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
 			window->Rectan(i, j);
 		}
 	} 
 	//Game();
 	//GameTest();
-	SDLTest();
 	return 0;
 }
 
