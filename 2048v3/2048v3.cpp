abf69f68eace679297b64735117df8caa2a21b52
skybloc429 <sto.lukas18@gmail.com>
2023-11-03 10:53:10 +0100

    Revert "dgsdg"
    This reverts commit bcf6e4a7b7dde3b45038a304f3edc3cd7f9ec1db.

--- 2048v3/2048v3.cpp
+++ 2048v3/2048v3.cpp
@@ -16,8 +16,8 @@ int main(int argc, char** argv)
 {
 	Window* window = new Window;
 	window->Create();
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
 			window->Rectan(i, j);
 		}
 	} 
